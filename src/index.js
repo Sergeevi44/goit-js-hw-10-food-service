89cff2afaa03bb6ed6ac5947e42be8a271529ec4
Oleksii <sergeevi44@gmail.com>
2021-08-10 18:25:26 +0300

    checkbox-js-add

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { classBody } = require("babel-types");
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+
+const body = document.querySelector('body');
+const changeTheme = document.querySelector('#theme-switch-toggle');
+
+body.classList.add(Theme.LIGHT);
+changeTheme.addEventListener('change', onChangeCheckBox);
+
+function onChangeCheckBox() {
+	body.classList.toggle(Theme.DARK);
+}
