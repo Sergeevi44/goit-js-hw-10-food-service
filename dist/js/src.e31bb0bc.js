3fade4edbc2585f661bf99da72af42d5669c7cec
Oleksii <sergeevi44@gmail.com>
2021-08-19 17:46:33 +0300

    checkbox-positin

--- dist/js/src.e31bb0bc.js
+++ dist/js/src.e31bb0bc.js
@@ -8904,12 +8904,30 @@ const Theme = {
 };
 const body = document.querySelector('body');
 const changeTheme = document.querySelector('#theme-switch-toggle');
-body.classList.add(Theme.LIGHT);
+getStorageData(); //  body.classList.add(Theme.LIGHT);
+
 changeTheme.addEventListener('change', onChangeCheckBox);
 
 function onChangeCheckBox() {
   body.classList.toggle(Theme.DARK);
   sessionStorage.setItem('current-theme', body.getAttribute('class'));
+
+  if (body.classList.contains(Theme.DARK)) {
+    changeTheme.setAttribute('checked', 'true');
+    sessionStorage.setItem('checkbox-condition', changeTheme.getAttribute('checked'));
+  } else {
+    changeTheme.setAttribute('checked', 'false');
+    sessionStorage.setItem('checkbox-condition', changeTheme.getAttribute('checked'));
+  }
+}
+
+function getStorageData() {
+  const savedData = sessionStorage.getItem('current-theme');
+
+  if (savedData) {
+    body.classList.add(savedData);
+    changeTheme.setAttribute('checked', sessionStorage.getItem('checkbox-condition'));
+  }
 }
 },{"babel-types":"../node_modules/babel-types/lib/index.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -8939,7 +8957,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49179" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51612" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -8,14 +8,32 @@ const body = document.querySelector('body');
 const changeTheme = document.querySelector('#theme-switch-toggle');
 
 
- body.classList.add(Theme.LIGHT);
+getStorageData();
 
 
- changeTheme.addEventListener('change', onChangeCheckBox);
+//  body.classList.add(Theme.LIGHT);
+
+
+changeTheme.addEventListener('change', onChangeCheckBox);
 
 function onChangeCheckBox() {
 	 
 	 body.classList.toggle(Theme.DARK);
 	 
-	 sessionStorage.setItem('current-theme', body.getAttribute('class'));
+	sessionStorage.setItem('current-theme', body.getAttribute('class'));
+	if (body.classList.contains(Theme.DARK)) {
+		changeTheme.setAttribute('checked', 'true');
+		sessionStorage.setItem('checkbox-condition', changeTheme.getAttribute('checked'));
+	}
+	else {
+		changeTheme.setAttribute('checked', 'false');
+		sessionStorage.setItem('checkbox-condition', changeTheme.getAttribute('checked'));
+	}
+}
+function getStorageData() {
+	const savedData = sessionStorage.getItem('current-theme');
+	if (savedData) {
+		body.classList.add(savedData);
+		changeTheme.setAttribute('checked', sessionStorage.getItem('checkbox-condition'));
+	}
 }
